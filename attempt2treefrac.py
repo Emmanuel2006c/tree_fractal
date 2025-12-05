7487903afd4b2a94efdf776ccca53b011bcb2a78
Emmanuel2006c <62173368+Emmanuel2006c@users.noreply.github.com>
2025-12-05 16:16:17 +0000

    Add files via upload
    keine Rekursion

---
+++ attempt2treefrac.py
@@ -0,0 +1,29 @@
+import math
+import matplotlib.pyplot as plt
+
+def plot_complex(origin, end):
+    x = [origin.real, end.real]
+    y = [origin.imag, end.imag]
+    plt.plot(x, y, linewidth=2, color="green")
+
+def tree(origin, branch, angle, scale, rec_factor):
+    if rec_factor > 0:
+        plot_complex(origin, branch)
+
+        branch_l = ((branch - origin) * math.e ** (1j * angle) * scale) + branch
+        branch_r = ((branch - origin) * math.e ** (-1j * angle) * scale) + branch
+
+        tree(branch, branch_l, angle, scale, rec_factor - 1)
+        tree(branch, branch_r, angle, scale, rec_factor - 1)
+
+origin = 0 + 0j
+first_end = 0 + 0.7j
+angle = math.pi / 4.123234
+scale = 0.7
+recursion_factor = 11
+
+tree(origin, first_end, angle, scale, recursion_factor)
+
+plt.axis("equal")
+plt.axis("off")
+plt.show()
